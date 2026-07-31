368bdb46a33641ba2628f01b7a25f3f6d534be6f
Hector Acosta <1Hector.AC@gmail.com>
2026-07-31 15:44:16 -0700

    Added create view and related changes. Also added ModelItem Index page to nav bar.

--- src/MediasDelight.Web/Controllers/MediaItemController.cs
+++ src/MediasDelight.Web/Controllers/MediaItemController.cs
@@ -1,7 +1,10 @@
 
+using MediasDelight.Web.Models;
+using MediasDelight.Web.Models.ViewModels;
 using MediasDelight.Web.Services;
-using MediasDelight.Web.Services.Implementations;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 namespace MediasDelight.Web.Controllers;
 
@@ -17,6 +20,29 @@ public class MediaItemController: Controller
     {
         var items = await _service.GetAllAsync();
         return View(items);
+    }
+
+    public async Task<IActionResult> Create()
+    {
+        List<MediaType> list = [new MediaType{Id = 1, Name= "Show"},new MediaType{Id = 2, Name= "Movie"},new MediaType{Id = 3, Name= "videoGame"}];
+        ViewBag.mediaTypes = new SelectList(list, "Id", "Name");
+        return View();
+
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateMediaItemViewModel viewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            List<MediaType> list = [new MediaType{Id = 1, Name= "Show"},new MediaType{Id = 2, Name= "Movie"},new MediaType{Id = 3, Name= "videoGame"}];
+            ViewBag.mediaTypes = new SelectList(list, "Id", "Name");
+            return View(viewModel);
+        }
+        Console.WriteLine($"Rating: {viewModel.MediaTypeId}");
+
+
+        return RedirectToAction("Index");
     }
 
 
---
+++ src/MediasDelight.Web/Models/ViewModels/CreateMediaItemViewModel.cs
@@ -0,0 +1,24 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace MediasDelight.Web.Models.ViewModels;
+
+public class CreateMediaItemViewModel
+{
+
+    [Required]
+    public int? MediaTypeId {get; set;}
+
+    [Required]
+    [StringLength(128)]
+    public required string Name {get ; set;}
+
+    [Required]
+    [Range(0,10)]
+    public int Rating {get; set;}
+
+    [StringLength(500)]
+    public string? Description {get; set;}
+
+
+}
