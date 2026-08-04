32dc01bcf588a95a298676dfd376a609f4dc03a7
Hector Acosta <1Hector.AC@gmail.com>
2026-08-04 09:52:08 -0700

    Change to AnalyzeMe page, added MediaType selection handling.

--- src/MediasDelight.Web/Controllers/AiFeatureController.cs
+++ src/MediasDelight.Web/Controllers/AiFeatureController.cs
@@ -1,5 +1,6 @@
 
 using MediasDelight.Web.Services;
+using MediasDelight.Web.Services.Implementations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.SolutionPersistence.Model;
 
@@ -8,29 +9,46 @@ namespace MediasDelight.Web.Controllers;
 public class AiFeatureController: Controller
 {
     private readonly GeminiService _geminiService;
+    private readonly IMediaTypeService _mediaTypeService;
 
-    public AiFeatureController(GeminiService geminiService)
+    public AiFeatureController(GeminiService geminiService, IMediaTypeService mediaTypeService)
     {
         _geminiService = geminiService;
+        _mediaTypeService = mediaTypeService;
     }
     
     public async Task<IActionResult> AnalyzeMe()
     {
-        return View();
+        var mediaTypes = await _mediaTypeService.GetAllAsync();
+
+
+        return View(mediaTypes);
     }
 
     [HttpPost]
-    public async Task<string> GenerateResponse()
+    public async Task<IActionResult> GenerateAnalyzeMeResponse([FromBody] int mediaTypeId)
     {
+
+        if (!ModelState.IsValid)
+        {
+            return BadRequest("ModelType Id wasnt valid: " + mediaTypeId);
+        }
+
+        
+
+        Console.WriteLine("id: " + mediaTypeId);
         try
         {
-            string test = "Describe what a giraffe looks like";
-            var result = await _geminiService.GenerateTextAsync(test);
-            return result;
+            // Test of Api call, remove later
+            // test = "Describe what a giraffe looks like";
+            //var result = await _geminiService.GenerateTextAsync(test);
+
+            var result = "You have too much free time. Touch grass instead.";
+            return Json(new {response = result});
         }
-        catch(Exception ex)
+        catch (Exception)
         {
-            return "API call error.";
+            return BadRequest("API response error.");
         }    
     }
 
