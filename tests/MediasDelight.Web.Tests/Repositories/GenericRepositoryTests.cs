12e66f6adf3c34a2af2565d704882df937bef433
Hector Acosta <1Hector.AC@gmail.com>
2026-08-11 09:33:48 -0700

    Added tests in GenericRepostioryTests.

--- tests/MediasDelight.Web.Tests/Repositories/GenericRepositoryTests.cs
+++ tests/MediasDelight.Web.Tests/Repositories/GenericRepositoryTests.cs
@@ -15,16 +15,86 @@ public class GenericRepositoryTests
         var repo = RepositoryHelper.Create<MediaItem>(out var context);
 
         await context.MediaItems.AddRangeAsync(
-            new MediaItem {Id=1, MediaTypeId = 1, UserId = "1", Name="Stuff", Rating=5, TimeStamp = new DateTime(2025, 5,5)},
-            new MediaItem {Id=2, MediaTypeId = 1, UserId = "1", Name="Stuff", Rating=5, TimeStamp = new DateTime(2025, 5,5)}
+            new MediaItem { Id = 1, MediaTypeId = 1, UserId = "1", Name = "Stuff", Rating = 5, TimeStamp = new DateTime(2025, 5, 5) },
+            new MediaItem { Id = 2, MediaTypeId = 1, UserId = "1", Name = "Stuff", Rating = 5, TimeStamp = new DateTime(2025, 5, 5) }
         );
         await context.SaveChangesAsync();
-        
+
         // Act
         var query = await repo.Query().ToListAsync();
 
         // Assert
         Assert.Equal(2, query.Count);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_WithExistingId_ReturnMatchingMediaItem()
+    {
+        // Arrange
+        var repo = RepositoryHelper.Create<MediaItem>(out var context);
+
+        await context.MediaItems.AddRangeAsync(
+            new MediaItem { Id = 1, MediaTypeId = 1, UserId = "1", Name = "Stuff", Rating = 5, TimeStamp = new DateTime(2025, 5, 5) },
+            new MediaItem { Id = 2, MediaTypeId = 1, UserId = "1", Name = "Stuff2", Rating = 7, TimeStamp = new DateTime(2025, 5, 5) }
+        );
+
+        await context.SaveChangesAsync();
+
+        // Act
+        var id = 1;
+        var result = await repo.GetByIdAsync(id);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(id, result.Id);
+    }
+
+    [Fact]
+    public async Task AddAsync_WithNoMediaItems_AddMediaItemtoDatabase()
+    {
+        // Arrange
+        var repo = RepositoryHelper.Create<MediaItem>(out var context);
+        var mediaItem = new MediaItem { Id = 2, MediaTypeId = 1, UserId = "1", Name = "Stuff2", Rating = 7, TimeStamp = new DateTime(2025, 5, 5) };
+        // Act
+        await repo.AddAsync(mediaItem);
+        await repo.SaveChangesAsync();
+
+        // Assert
+        Assert.Equal(1, context.MediaItems.Count());
+    }
+
+    [Fact]
+    public async Task Remove_WithMediaItems_RemoveMediaItemFromDatabase()
+    {
+        // Arrange
+        var repo = RepositoryHelper.Create<MediaItem>(out var context);
+        var mediaItem = new MediaItem { Id = 2, MediaTypeId = 1, UserId = "1", Name = "Stuff2", Rating = 7, TimeStamp = new DateTime(2025, 5, 5) };
+        await repo.AddAsync(mediaItem);
+        await repo.SaveChangesAsync();
+
+        await context.SaveChangesAsync();
+
+        // Act
+        repo.Remove(mediaItem);
+        await repo.SaveChangesAsync();
+
+        // Assert
+        Assert.Empty(context.MediaItems);
+    }
+
+    [Fact]
+    public async Task SaveChangesAsync_WithPendingChanges_PersistDataToDatabase()
+    {
+        // Arrange
+        var repo = RepositoryHelper.Create<MediaItem>(out var context);
+        var mediaItem = new MediaItem { Id = 2, MediaTypeId = 1, UserId = "1", Name = "Stuff2", Rating = 7, TimeStamp = new DateTime(2025, 5, 5) };
+        
+        // Act
+        await repo.AddAsync(mediaItem);
+        await repo.SaveChangesAsync();
+
+        // Assert
+        Assert.NotEmpty(context.MediaItems);
     }
 
 }
